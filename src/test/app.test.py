5672159069a4b07abe7302cbc5544f9565c863ea
danwen <aaron117joy@gmail.com>
2018-02-24 17:14:35 +0800

    fix wrong list

--- src/test/app.test.py
+++ src/test/app.test.py
@@ -21,7 +21,7 @@ class AppTestCase(unittest.TestCase):
     def test_db(self):
         client = MongoClient('localhost', 27017)
         #test initial state of the database
-        self.assertEqual(client.database_names(), ['local', 'admin'])
+        self.assertEqual(client.database_names(), ['local'])
         #test insertion of an entry
         db = client.test_db
         collection = client.test_collection
@@ -29,14 +29,14 @@ class AppTestCase(unittest.TestCase):
         _id = collection.insert_one(entry).insert_id
         self.assertEqual(collection.find_one({'_id': ObjectId(id)})['text'], 'test')
         #test creation of the test database
-        self.assertEqual(client.database_names(), ['local', 'admin', 'test_db'])
+        self.assertEqual(client.database_names(), ['local', 'test_db'])
         #test removal of the entry
         collection.remove({'_id': ObjectId(id)})
         self.assertIsNone(collection.find_one({'_id': ObjectId(id)}))
         #test removal of the test database
         db.test_collection.drop()
         client.drop_database('test_db')
-        self.assertEqual(client.database_names(), ['local', 'admin'])
+        self.assertEqual(client.database_names(), ['local'])
 
 if __name__ == '__main__':
     if __package__ is None:
