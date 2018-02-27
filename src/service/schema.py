b4cc84e49a4df390fd2cf8a9668fd5a642017aa6
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2018-02-27 18:37:00 +0800

    Fix db fields

--- src/service/schema.py
+++ src/service/schema.py
@@ -6,16 +6,14 @@ class User(Document):
     username = StringField(required = True, unique = True)
     fullname = StringField(required = True)
     password = StringField(required = True)
-    createdOn = DateTimeField(required = True)
-    lastLogin = DateTimeField(required = True)
     age = IntField(required = True)
 
 class Post(Document):
     _id = IntField(required = True, primary_key = True)
     title = StringField(required = True)
     author = ReferenceField(User, reverse_delete_rule=CASCADE)
-    publishDate = DateTimeField(required = True)
-    isPublic = BooleanField(required = True)
+    publish_date = DateTimeField(required = True)
+    public = BooleanField(required = True)
     text = StringField(required = True)
 
 class Session(Document):
