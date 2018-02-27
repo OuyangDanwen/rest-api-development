b626ced33fe92c753c6ebb6b3c51bc4affad512d
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2018-02-28 01:19:22 +0800

    De-expose mongodb service

--- src/service/start_services.sh
+++ src/service/start_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apachectl start
-mongod --smallfiles --bind_ip_all --fork --logpath /log/mongod.log
+mongod --smallfiles --fork --logpath /log/mongod.log
 mongo diary_app --eval 'db.counter.insert({"value": 0})'
 python /service/app.py
