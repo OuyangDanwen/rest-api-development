b20fbdc68ff37c516cd9319042bb5b327ba46252
Zander Chai <xzandercx@gmail.com>
2018-02-28 03:30:30 +0800

    Registration API Call Successful, Skeleton Login

--- src/html/js/common.js
+++ src/html/js/common.js
@@ -11,10 +11,74 @@ function getUrlParameter(sParam) {
 
 /* AJAX Codes */
 
-//Registration
+// Registration
 $(document).ready(function(){
-		$('#Register_Form').on('submit', function(e){
-			//TODO: Validation
-			window.alert('Hi');
+	$('#Register_Form').on('submit', function(e){
+		e.preventDefault();
+		// TODO: Field Validation
+		
+		// Passwords Matching
+		if ($("#Password").val() != $("#CfmPassword").val()) {
+			alert("Passwords do not match.");
+			return;
+		}
+				
+		var formData = {
+			username: $("#Username").val(), 
+			password: $("#Password").val(), 
+			fullname: $("#Fullname").val(),
+			age: $("#Age").val()
+		};
+		var jsonData = JSON.stringify(formData);
+		$.ajax({
+			type: 'POST',
+			url: 'http://localhost:8080/users/register',
+			dataType: "json",
+			contentType: "application/json",
+			data: jsonData,
+			success: function(response) { // When status (true or false) happens during registration
+				if (response.status) {
+					alert("Registration Successful.");
+					window.location.href = "http://localhost";
+				} else {
+					alert(response.error);
+				}
+			},
+			error: function() { // When Bad Request Happens (error 400).
+				alert('Error. Please Try Again.');
+			}
 		});
 	});
+});
+
+// Login
+$(document).ready(function(){
+	$('#Register_Form').on('submit', function(e){
+		e.preventDefault();
+		
+		var formData = {
+			username: $("#Username").val(), 
+			password: $("#Password").val()
+		};
+		
+		var jsonData = JSON.stringify(formData);
+		$.ajax({
+			type: 'POST',
+			url: 'http://localhost:8080/users/authenticate',
+			dataType: "json",
+			contentType: "application/json",
+			data: jsonData,
+			success: function(response) { // When status (true or false) happens during login
+				if (response.status) {
+					alert("Welcome.".concat($("#Username").val()));
+					//window.location.href = "http://localhost";
+				} else {
+					alert("Invalid Username or Password.");
+				}
+			},
+			error: function() { // When Bad Request Happens (error 400).
+				alert('Error. Please Try Again.');
+			}
+		});
+	});
+});
