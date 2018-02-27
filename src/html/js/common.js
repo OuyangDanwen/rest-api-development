824f907729d803aa0a13f3818bfa37f79ae3ff0f
Zander Chai <xzandercx@gmail.com>
2018-02-28 04:55:55 +0800

    Registration, Login, Home Page API Call success.

--- src/html/js/common.js
+++ src/html/js/common.js
@@ -9,6 +9,106 @@ function getUrlParameter(sParam) {
     }
 }
 
+function loadHome() {
+	if (checkCookie()) {
+		
+		var uuid = { token: getCookie("uuid") };
+		var jsonData = JSON.stringify(uuid);
+		
+		$.ajax({ //retrieve user info and attempt to display it.
+			type: 'POST',
+			url: 'http://localhost:8080/users',
+			dataType: "json",
+			contentType: "application/json",
+			data: jsonData,
+			success: function(response) { // When status (true or false) happens during registration
+				if (response.status) {
+					document.getElementById("welcomeHeader").innerHTML = "Welcome, " + response.result.fullname;
+					document.getElementById("welcomeMsg").innerHTML = "and you are " + response.result.age + " years old.";
+				} else {
+					alert("Failed to retrieve info. Server down perhaps?");
+				}
+			},
+			error: function() { // When Bad Request Happens (error 400).
+				alert('Error. Please Try Again.');
+			}
+		});
+		
+		document.getElementById("welcomeHeader").innerHTML = "Welcome, ";
+	} else {
+		window.alert("Not Logged In...");
+		window.location.href = "http://localhost";
+	}	
+}
+
+function logout() {
+	var uuid = { token: getCookie("uuid") };
+	var jsonData = JSON.stringify(uuid);
+	
+	$.ajax({
+			type: 'POST',
+			url: 'http://localhost:8080/users/expire',
+			dataType: "json",
+			contentType: "application/json",
+			data: jsonData,
+			success: function(response) { // When status (true or false) happens during registration
+				if (response.status) {
+					destroyCookie();
+					alert("Logout Successful.");
+					window.location.href = "http://localhost";
+				} else {
+					alert("Failed to logout. Perhaps token already terminated previously?");
+					window.location.href = "http://localhost";
+				}
+			},
+			error: function() { // When Bad Request Happens (error 400).
+				alert('Error. Please Try Again.');
+			}
+	});
+}
+
+// Start Javascript Cookie Functions.. Insecure as f*ck.
+// Taken from https://www.w3schools.com/js/js_cookies.asp
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+    d.setTime(d.getTime() + (exdays * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) { //returns cookie value
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("uuid");
+    if (user == "") { // no uuid cookie detected
+       	return false;
+    } else {
+		return true;
+	}
+}	
+
+function destroyCookie() {	
+	document.cookie = "uuid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+// End of Javascript Cookie functions
+
+
 /* AJAX Codes */
 
 // Registration
@@ -53,7 +153,7 @@ $(document).ready(function(){
 
 // Login
 $(document).ready(function(){
-	$('#Register_Form').on('submit', function(e){
+	$('#Login_Form').on('submit', function(e){
 		e.preventDefault();
 		
 		var formData = {
@@ -70,8 +170,9 @@ $(document).ready(function(){
 			data: jsonData,
 			success: function(response) { // When status (true or false) happens during login
 				if (response.status) {
-					alert("Welcome.".concat($("#Username").val()));
-					//window.location.href = "http://localhost";
+					alert("UUID Value (Check with Cookie Value): " + response.result.token); // TO REMOVE THIS. FOR DEBUG PURPOSES ONLY
+					setCookie("uuid", response.result.token, 30); //30 minutes expiry
+					window.location.href = "http://localhost/home.html";
 				} else {
 					alert("Invalid Username or Password.");
 				}
